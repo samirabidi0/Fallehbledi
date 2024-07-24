f78392c562f213736fa67b0db06b8c4c7c959d06
samirabidi0 <abi.sam010101@gmail.com>
2024-07-24 08:11:40 +0100

    auth sign up farmer

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 const express = require ('express')
 const dotenv = require("dotenv")
+const cors = require('cors');
 const  {pricesData} = require('./helpers/pricesData')
 const prisma = require("./database/prisma")
+const bodyParser = require('body-parser')
+const app = express();
+const routersAuthfarmer = require("./routes/Authfarmer")
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //Load environment variables from .env file
 dotenv.config();
-const routersAuthfarmer = require("./routes/Authfarmer.js")
+
 
 //Declare the express app
-const app = express();
+
 
 //Set port
 const port = 5000;
