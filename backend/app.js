b45c4817a09687c5582775161a0845ed77fd946c
fathi777b <barhoumifathi2021@gmail.com>
2024-07-24 16:23:13 +0200

    update farmer

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,21 @@ const express = require ('express')
 const dotenv = require("dotenv")
 const  {pricesData} = require('./helpers/pricesData')
 const prisma = require("./database/prisma")
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//Load environment variables from .env file
-dotenv.config();
 const routersAuthfarmer = require("./routes/Authfarmer.js")
-
+const cors = require ('cors')
 //Declare the express app
 const app = express();
 
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //Set port
 const port = 5000;
+//Load environment variables from .env file
+dotenv.config();
+
+
 
 app.get('/prices',pricesData);
 app.use('/api/farmer',routersAuthfarmer)
