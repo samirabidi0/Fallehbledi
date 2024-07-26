cd055200e5c810545a753ed9a2ee321a375f9046
achrefmehri <ashmaymh@gmail.com>
2024-07-26 09:20:29 +0100

    finishing home page and start working in farmer dashboard

---
+++ frontend/src/app/dash/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/dash/Navbar"
+import SideBar from "@/components/dash/sideBar"
+const page = () => {
+  return (
+    <div>
+        <Navbar />
+        <SideBar />
+    </div>
+  )
+}
+
+export default page
