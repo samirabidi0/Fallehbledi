8f48a31fe633134bfb789ba5223a4e4d8f768c50
fathi777b <barhoumifathi2021@gmail.com>
2024-07-27 16:59:29 +0200

    handling function GetFarmtoolByName in back end

--- backend/routes/FarmerTools.js
+++ backend/routes/FarmerTools.js
@@ -5,10 +5,11 @@ const router = express.Router();
 const { CreateFarmtool  } = require('../controllers/FarmerTools.js')
 const { GetAllFarmtools } = require('../controllers/FarmerTools.js')
 const { DeleteFarmtool } = require('../controllers/FarmerTools.js')
-const {GetFarmtoolByName} = require
+const {GetFarmtoolByName} = require("../controllers/FarmerTools.js")
 
 router.post("/add" ,  CreateFarmtool)
 router.get("/all", GetAllFarmtools)
 router.delete("/:id", DeleteFarmtool)
+router.get("/:name" ,GetFarmtoolByName)
 
 module.exports = router
