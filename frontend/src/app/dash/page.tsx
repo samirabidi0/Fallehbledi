7b23de67778d39b4ef843aaf35f3c3af0ea2bdf0
achrefmehri <ashmaymh@gmail.com>
2024-07-31 09:12:29 +0100

    refactor farmer dash

--- frontend/src/app/dash/page.tsx
+++ frontend/src/app/dash/page.tsx
@@ -1,7 +1,4 @@
-import Navbar from "@/components/dash/Navbar"
-import SideBar from "@/components/dash/sideBar"
-import Weather from './weather'
-import Marketprices from "./marketPrice"
+
 import {cookies} from 'next/headers'
 
 const page = () => {
@@ -10,22 +7,6 @@ const page = () => {
   
   return (
     <div className="">
-        <Navbar />
-        <SideBar />
-        <div className=" ml-[15rem]">
-        <Marketprices />
-
-</div>
-        <div className=" flex justify-between ">
-        <div className=" ml-[20rem] flex flex-col space-y-5">
-          <h1 className="text-3xl font-bold mt-6 text-gray-900 ">Welcome <span className="text-[#058f1a]">UserName !</span></h1>
-          <p className="w-[37rem]">Explore the features, check the <span className="text-[#058f1a]">weather</span>, and get the lastest <span className="text-[#058f1a]">market prices.</span> Your journey begins here. Let's make the most of it together!</p>
-        </div>
-        <Weather/>
-        </div>
-        
-
-        
     </div>
 
   )
--- frontend/src/components/navbar/profileMenu.tsx
+++ frontend/src/components/navbar/profileMenu.tsx
@@ -1,31 +1,56 @@
 'use client'
+import ProfileImage from '../../../public/image/blank-profile-picture-973460_960_720.webp'
+import Link from 'next/link'
 import Image from 'next/image'
-import ProfileImage from '../../../public/image/profile.jpg'
-import { useState } from 'react'
+import { useActive } from '../context/activeContext'
+import { useRef } from 'react'
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp  } from "react-icons/fa6";
+import { TiWeatherCloudy } from "react-icons/ti";
+
+
 const profileMenu = () => {
-    const [showMenu,setShowMenu]=useState(false)
+  const { active, setActive } = useActive();
+const menuRef = useRef<HTMLDivElement>();
+const imgRef = useRef()
+window.addEventListener('click',(e)=>{
+  if(e.target !== menuRef.current && e.target !== imgRef.current){
+    setActive(false)
+  } 
+})
   return (
-<div id="userDropdown" className=" absolute ml-[73rem]  bg-white  divide-gray-100 rounded-lg shadow w-44 ">
-    <div className="px-4 py-3 text-sm text-gray-900 ">
+    <>
+    <div onClick={()=>setActive(!active)} className= " flex items-center cursor-pointer  transition mr-[6rem]">
+       
+           <Image ref={imgRef} src={ProfileImage}  className="w-10 h-10 rounded-full mr-2 cursor-pointer"  alt="User dropdown"/>
+  
+       {active && <FaAngleUp />}
+       {!active && <FaAngleDown/>}
+    </div>   
+    {active && <div id="user-dropdown" className="fixed mt-2 bg-white  divide-gray-100 rounded-lg shadow w-44 ">
+    
+    <div ref={menuRef} className="px-4 py-3 text-sm text-gray-900 ">
       <div>Bonnie Green</div>
       <div className="font-medium truncate">name@flowbite.com</div>
     </div>
-    <ul className="py-2 text-sm text-gray-900 dark:text-gray-200" aria-labelledby="avatarButton">
+    <ul className="py-2 text-sm text-gray-900 " aria-labelledby="user-menu-button">
       <li>
-        <a href="#" className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Dashboard</a>
+        <a href="#" className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Weather</a>
+      </li>
+      <li>
+        <Link href="/dash/marketprices" className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Market Prices</Link>
       </li>
       <li>
         <a href="#" className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Settings</a>
       </li>
-      <li>
-        <a href="#" className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Earnings</a>
-      </li>
     </ul>
     <div className="py-1">
-      <a href="#"className="block px-4 py-2 text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Sign out</a>
+      <a href="#"className="block px-4 py-2 text-sm text-gray-900 hover:bg-[#058f1a] hover:text-[#fff]">Sign out</a>
     </div>
-</div>
-    
+</div>}
+        
+    </>
+
   )
 }
 
